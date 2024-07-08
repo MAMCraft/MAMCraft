9d48771e396e45db18aeb1647f1dfc6fc3097c50
jyunseon81 <102278574+jyunseon81@users.noreply.github.com>
2024-07-08 17:49:21 +0900

    Add Prototype

--- Source/JYS/Private/ArrowAttack.cpp
+++ Source/JYS/Private/ArrowAttack.cpp
@@ -3,8 +3,19 @@
 
 #include "ArrowAttack.h"
 #include "Components/BoxComponent.h"
+#include "Engine/DamageEvents.h"
 
+void AArrowAttack::OnrightWeaponCollision(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	UE_LOG(LogTemp, Warning, TEXT("OnrightWeaponCollision : %s"), *OtherActor->GetName());
+	if (OtherActor->ActorHasTag("Enemy"))
+	{
+		FDamageEvent damageEvent;
+		OtherActor->TakeDamage(1, damageEvent,nullptr, this);
+		Destroy();
+	}
 
+}
 // Sets default values
 AArrowAttack::AArrowAttack()
 {
@@ -33,6 +44,7 @@ void AArrowAttack::BeginPlay()
 {
 	Super::BeginPlay();
 	GetWorldTimerManager().SetTimer(DestroyTimerHandle, this, &AArrowAttack::DestroyArrow, 1.0f, true);
+	BoxComp->OnComponentBeginOverlap.AddDynamic(this, &AArrowAttack::OnrightWeaponCollision);
 }
 
 // Called every frame
--- Source/JYS/Public/ArrowAttack.h
+++ Source/JYS/Public/ArrowAttack.h
@@ -39,6 +39,7 @@ public:
 	UPROPERTY(EditAnywhere)
 	class APlayerCharacter* PlayerCharacter2;
 
-
+	UFUNCTION()
+	void OnrightWeaponCollision(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 };
 

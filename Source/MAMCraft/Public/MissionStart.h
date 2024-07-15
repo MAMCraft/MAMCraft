77d4348ad7fcf48c94a1e87257d90cc9942caf29
yeanha <143509984+yeanha@users.noreply.github.com>
2024-07-15 10:47:44 +0900

    시작 미션 UI 추가, gate 수

---
+++ Source/MAMCraft/Public/MissionStart.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MissionStart.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MAMCRAFT_API UMissionStart : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};

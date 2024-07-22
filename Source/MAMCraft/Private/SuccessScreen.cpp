336cd247d6be77abb846cdaa62b40ec029772716
jyunseon81 <102278574+jyunseon81@users.noreply.github.com>
2024-07-22 15:18:11 +0900

    최종

--- Source/MAMCraft/Private/SuccessScreen.cpp
+++ Source/MAMCraft/Private/SuccessScreen.cpp
@@ -9,11 +9,45 @@
 #include "MediaPlayer.h"
 #include "MediaTexture.h"
 #include "MediaSource.h"
+#include "Components/CanvasPanelSlot.h"
+#include "Kismet/GameplayStatics.h"
 
 void USuccessScreen::NativeConstruct()
 {
 	Super::NativeConstruct();
 
+}
+void USuccessScreen::StartButtonCallback()
+{
+
+	PlayVideo();
+	UCanvasPanelSlot* CanvasSlot = Cast<UCanvasPanelSlot>(VideoPlayer->Slot);
+	if (CanvasSlot)
+	{
+		CanvasSlot->SetZOrder(1);
+	}
+	GetWorld()->GetTimerManager().SetTimer(TimerHandle1, this, &USuccessScreen::TestFunction, 10.f);
+
+}
+void USuccessScreen::TestFunction()
+{
+	//PlayVideo();
+	//UCanvasPanelSlot* CanvasSlot = Cast<UCanvasPanelSlot>(VideoPlayer->Slot);
+	//if (CanvasSlot)
+	//{
+	//	CanvasSlot->SetZOrder(1);
+	//}
+
+	// 레벨을 바꿔주는 함수
+	UGameplayStatics::OpenLevel(this, TEXT("Start"));
+	this->SetVisibility(ESlateVisibility::Hidden);
+}
+
+void USuccessScreen::Test()
+{
+	// 레벨을 바꿔주는 함수
+	UGameplayStatics::OpenLevel(this, TEXT("Start"));
+	this->SetVisibility(ESlateVisibility::Hidden);
 }
 
 
--- Source/MAMCraft/Public/SuccessScreen.h
+++ Source/MAMCraft/Public/SuccessScreen.h
@@ -22,10 +22,25 @@ protected:
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Media")
 	class UMediaSource* MediaSource;
+
+		UPROPERTY(VisibleAnywhere, BlueprintReadWrite, meta = (BindWidget))
+	class UImage* VideoPlayer;
 public:
+	UFUNCTION(BlueprintCallable, Category = "Media")
+	virtual void StartButtonCallback();
 
 	virtual void NativeConstruct();
 	UFUNCTION(BlueprintCallable, Category = "Media")
-	void PlayVideo();
+	virtual void PlayVideo();
+
+	FTimerHandle TimerHandle;
+	FTimerHandle TimerHandle1;
+
+	UFUNCTION(BlueprintCallable, Category = "Media")
+	virtual void TestFunction();
+	UFUNCTION()
+	virtual void Test();
+
+
 
 };
